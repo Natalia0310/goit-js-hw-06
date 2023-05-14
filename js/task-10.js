4a0b6aa421aba99a5923c4d3983986f7cccbbc2d
Natalia0310 <ndemydenko@ukr.net>
2023-05-14 16:12:10 +0300

    10 swoe

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+const button =document.querySelector(".js-button");
+const container =document.querySelector(".js-container");
+
+button.addEventListener("click", onclick);
+step=0
+function onclick(evt) {
+  step+=5px;
+  container.style.marginLeft=`${step}`px;
+  console.log(evt.currentTarget);
 }
